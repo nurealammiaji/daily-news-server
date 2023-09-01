6cc440ed3c42ae6c9648fc1835de52897465669e
nurealammiaji <nurealam.miaji@gmail.com>
2023-09-02 00:26:20 +0600

    data added

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const news = require("./data/news.json");
+const categories = require("./data/categories.json");
 const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
     res.send("Daily News Server")
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
 })
 
 app.listen(port, () => {
